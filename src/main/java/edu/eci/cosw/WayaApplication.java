cde9e30eac17617842a1166baacdb7cc8f3c263b
Efren <efrensubdy@gmail.com>
2017-02-10 11:50:05 -0500

    Deberia servir

---
+++ src/main/java/edu/eci/cosw/WayaApplication.java
@@ -0,0 +1,12 @@
+package edu.eci.cosw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WayaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WayaApplication.class, args);
+	}
+}
