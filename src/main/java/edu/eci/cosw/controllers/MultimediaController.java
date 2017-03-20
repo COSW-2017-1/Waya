f055c63ee513bb3f2ce64964fc637caa7f5b764e
David <david.useche@mail.escuelaing.edu.co>
2017-03-20 18:24:56 -0500

    Funcionalidad basica controller multimedia, falta hacerle autowired y deberia servir

--- src/main/java/edu/eci/cosw/controllers/MultimediaController.java
+++ src/main/java/edu/eci/cosw/controllers/MultimediaController.java
@@ -4,8 +4,11 @@ package edu.eci.cosw.controllers;
 import edu.eci.cosw.entities.Multimedia;
 import edu.eci.cosw.entities.MultimediaId;
 import edu.eci.cosw.services.MultimediaServices;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * Created by david on 17/03/2017.
@@ -13,15 +16,38 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/multimedia")
 public class MultimediaController {
+
     //@Autowired
     private MultimediaServices multimediaServices;
 
-    @RequestMapping(path = "/{numero}", method = RequestMethod.GET)
+    @RequestMapping(path = "/{bar}/{numero}", method = RequestMethod.GET)
     @ResponseBody
-    public ResponseEntity<Multimedia> getMultimedia(@PathVariable("numero") int numero, @PathVariable("bar")int bar) {
+    public ResponseEntity<Multimedia> getMultimedia(@PathVariable int numero, @PathVariable int bar) {
         MultimediaId a=new MultimediaId(bar,numero);
+        Multimedia toReturn = multimediaServices.getById(a);
+        if(toReturn == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return ResponseEntity.ok().body(toReturn);
 
-        return ResponseEntity.ok().body(multimediaServices.getById(a));
+    }
 
+    @RequestMapping(path = "/{bar}", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<List<Multimedia>> getAllMultimediaBar(@PathVariable int bar){
+        List<Multimedia> toReturn = multimediaServices.getMultimediaByBar(bar);
+        if(toReturn == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return ResponseEntity.ok().body(toReturn);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<?> postMultimedia(@RequestBody Multimedia multimedia){
+        if(multimediaServices.getById(multimedia.getId())!=null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        multimediaServices.SaveMultimedia(multimedia);
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
     }
 }
