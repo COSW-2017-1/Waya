19479ee98d3a92c0be5c8d161fd89b93679d2a40
bauto17 <bauto17@gmail.com>
2017-03-29 07:09:53 -0500

    bares front

--- src/main/resources/static/app/detalleBar/detalleBar.js
+++ src/main/resources/static/app/detalleBar/detalleBar.js
@@ -3,13 +3,16 @@
 angular.module('myApp.detalleBar', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/detalleBar', {
+  $routeProvider.when('/detalleBar/:nBar', {
     templateUrl: 'detalleBar/detalleBar.html',
     controller: 'DetalleBarCtrl'
   });
 }])
 
-.controller('DetalleBarCtrl', ['$scope', 'bares', function ($scope,bares)  {
-
-
+.controller('DetalleBarCtrl', ['$scope', 'bares', '$routeParams', function ($scope,bares,$routeParams)  {
+    var nBar =$routeParams.nBar;
+    bares.get({id:nBar}, function(bar) {
+        console.log(bar);
+        $scope.bar=bar;
+    });
 }]);
